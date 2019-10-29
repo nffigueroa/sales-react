f4b0343cd054fd929736d44acc43dc6d92f7d0fc
Fabian <nffigueroag@gmail.com>
2019-10-28 20:49:06 -0600

    Just as a render prop RowTableMolecule

--- src/components/molecules/table/rowTable.js
+++ src/components/molecules/table/rowTable.js
@@ -8,20 +8,7 @@ export const RowTableMolecule = (props) => {
     const {trTd} =  props;
     return  <>
     <div className="row-table">
-                {/* {
-                    trTd.map((item, index) => {
-                        switch (item.type) {
-                            case typeItemTable.text:
-                                return <ItemTDTable key={index} td={item.value}></ItemTDTable>
-                            case typeItemTable.delete:
-                                return <i key={index} className="fas fa-trash-alt"></i>
-                            case typeItemTable.update:
-                                return <i key={index} className="fas fa-pen"></i>
-                            default:
-                                return <span>No hay nada</span>
-                        }
-                    })
-                } */}
+                {props.children}
             </div>
             </>
 }
