48b3c0f37f067dba5880263de87a0c26af2889c2
Fabian <nffigueroag@gmail.com>
2019-10-28 20:49:46 -0600

    Fix the ui issue for render the info

--- src/components/organism/table.js
+++ src/components/organism/table.js
@@ -3,27 +3,33 @@ import React from 'react';
 import { RowTableMolecule } from '../molecules/table/rowTable';
 import { typeItemTable } from '../../util/const';
 
-// export const mockTable = [
-//     {
-//         value: 'Gaseosa',
-//         type: typeItemTable.text
-//     },
-//     {
-//         value: 'Bebidas',
-//         type: typeItemTable.text
-//     },
-//     {
-//         value: 'Eliminar',
-//         type: typeItemTable.delete
-//     },
-//     {
-//         value: 'Actualizar',
-//         type: typeItemTable.update
-//     }
-// ]
+import './table.scss';
+
 
 const TableOrganism = (props) => {
-    return <RowTableMolecule trTd={props.data}></RowTableMolecule>;
+return <>
+    <table> 
+    <thead>
+        <tr>
+            {props.label.map((item) => {
+                return <th>{item.label}</th>
+            })} 
+        </tr>
+        </thead>
+        <tbody>
+            {props.data[0].map((element, index) => {
+                return (<tr className="row-table-item">
+                        {Object.keys(element).map((item, index) => {
+                            return <td>{element[Object.keys(element)[index]]}</td>
+                        })}
+                    </tr>)
+            })}
+
+        </tbody>
+    </table>
+    
+    </>
 }
+
  
 export default TableOrganism;
