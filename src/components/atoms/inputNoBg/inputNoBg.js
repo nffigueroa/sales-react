3a13a5cec32bbbe95d5614941a047112e96c1b1e
Fabian <nffigueroag@gmail.com>
2019-11-08 21:24:35 -0500

    Some changes for the modal

--- src/components/atoms/inputNoBg/inputNoBg.js
+++ src/components/atoms/inputNoBg/inputNoBg.js
@@ -10,7 +10,7 @@ import './inputNoBg.scss';
 import React from 'react';
 
 const InputNoBgComponent = (props) => {
-    return ( <input onClick={() => props.customFunction()} className="inputNotBg" type={props.type} id={props.id} placeholder={props.placeholder} onChange={(e) => props.getResultFromInput({value: e.target.value, id: e.target.id})} />);
+    return ( <input className="inputNotBg" type={props.type} id={props.id} placeholder={props.placeholder} onChange={(e) => props.getResultFromInput({value: e.target.value, id: e.target.id})} />);
 }
  
 export default InputNoBgComponent;
